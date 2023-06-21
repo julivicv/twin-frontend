ab752bfd624dad1ac37c1669702f77e38909d786
julivicv <102142918+julivicv@users.noreply.github.com>
2023-06-21 09:48:33 -0300

    fixed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
   </q-page>
 </template>
 
 <script lang="ts">
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
   setup() {
     const todos = ref([
       {
