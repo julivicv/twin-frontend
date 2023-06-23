48fb929e17b056d4997cb399b88377cd884af198
julivicv <102142918+julivicv@users.noreply.github.com>
2023-06-23 11:00:50 -0300

    Adjustments on the login form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,86 +1,89 @@
 <template>
-  <q-page class="column items-center justify-evenly">
-    <div
-      class="tw-flex tw-items-center tw-w-[60%] tw-justify-center form-container self-start"
-    >
-      <q-form
-        @submit="onSubmit"
-        @reset="onReset"
-        class="q-gutter-md login-form"
+  <q-layout>
+    <q-page class="column items-center justify-evenly">
+      <img class="absolute-top-left" src="../assets/twinny_left.svg" alt="" />
+      <div
+        class="tw-flex tw-items-center tw-w-[60%] tw-justify-center form-container self-start"
       >
-        <q-input
-          filled
-          v-model="login"
-          label="login"
-          lazy-rules
-          :rules="[
-            (val) =>
-              (val && val.length > 0) ||
-              'Por favor, digite um login de acesso.',
-          ]"
-        />
+        <q-form @submit="onSubmit" class="q-gutter-md login-form tw-w-[40%]">
+          <q-input
+            standout
+            rounded
+            v-model="login"
+            label="login"
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val && val.length > 0) ||
+                'Por favor, digite um login de acesso.',
+            ]"
+          >
+            <template v-slot:prepend>
+              <q-icon name="person" />
+            </template>
+          </q-input>
 
-        <q-input
-          filled
-          v-model="senha"
-          label="Senha"
-          lazy-rules
-          :rules="[
-            (val) =>
-              (val !== null && val !== '') ||
-              'Por favor, digite uma senha de acesso.',
-            (val) =>
-              (val > 0 && val < 100) || 'Senha inválida ou insuficiente.',
-          ]"
-        />
+          <q-input
+            standout
+            rounded
+            v-model="senha"
+            label="Senha"
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val !== null && val !== '') ||
+                'Por favor, digite uma senha de acesso.',
+              (val) =>
+                (val > 0 && val < 100) || 'Senha inválida ou insuficiente.',
+            ]"
+          >
+            <template v-slot:prepend>
+              <q-icon name="lock" />
+            </template>
+          </q-input>
 
-        <q-btn
-          label="Termos e condições"
-          color="accent"
-          @click="animateBubbles"
-        />
-
-        <q-toggle v-model="accept" label="Aceito a licença e os termos." />
-
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-          <q-btn
-            label="Reset"
-            type="reset"
-            color="primary"
-            flat
-            class="q-ml-sm"
-          />
+          <div class="flex">
+            <q-btn
+              class="tw-w-[40%] tw-rounded-[1.5rem] tw-mx-auto"
+              label="Logar-se"
+              type="submit"
+              color="primary"
+            />
+          </div>
+        </q-form>
+      </div>
+      <div class="bubbles-container">
+        <div class="title-container tw-absolute z-1">
+          <h1 class="title">Bem Vindo(a)!</h1>
+          <span class="subtitle">Faça o login para continuar</span>
         </div>
-      </q-form>
-    </div>
-    <div class="bubbles-container">
-      <img
-        ref="top"
-        class="bubbles btop"
-        src="../assets/bubbles_top.svg"
-        alt=""
-      />
-      <img
-        ref="right"
-        class="bubbles bright"
-        src="../assets/bubbles_right.svg"
-        alt=""
-      />
-      <img
-        ref="bottom"
-        class="bubbles bbottom"
-        src="../assets/bubbles_bottom.svg"
-        alt=""
-      />
-    </div>
-  </q-page>
+        <img
+          ref="top"
+          class="bubbles btop"
+          src="../assets/bubbles_top.svg"
+          alt=""
+        />
+        <img
+          ref="right"
+          class="bubbles bright"
+          src="../assets/bubbles_right.svg"
+          alt=""
+        />
+        <img
+          ref="bottom"
+          class="bubbles bbottom"
+          src="../assets/bubbles_bottom.svg"
+          alt=""
+        />
+      </div>
+    </q-page>
+  </q-layout>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue';
 import { useQuasar } from 'quasar';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 import anime from 'animejs';
 
@@ -116,14 +119,14 @@ export default {
           anime({
             targets: '.bubbles',
             scale: function () {
-              return ['100%', anime.random(101, 105) + '%'];
+              return ['100%', anime.random(104, 109) + '%'];
             },
             duration: function () {
-              return anime.random(1500, 1800);
+              return anime.random(1600, 1900);
             },
             direction: 'alternate',
             loop: true,
-            easing: 'easeInOutSine',
+            easing: 'easeInOutQuad',
           })
         );
     },
@@ -140,6 +143,7 @@ export default {
     const top = ref(false);
     const right = ref(false);
     const bottom = ref(false);
+    $q.dark.set(true);
 
     return {
       login,
@@ -166,38 +170,53 @@ export default {
           });
         }
       },
-      onReset() {
-        login.value = null;
-        senha.value = null;
-        accept.value = false;
-      },
     };
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 .form-container {
-  background-image: linear-gradient(to bottom right, #202c59, #d95d39);
-  height: calc(100vh - 50px);
+  background-image: linear-gradient(to top right, $secondary, $primary);
+  height: 100vh;
 }
 .bubbles {
   position: absolute;
 }
 .btop {
   top: -110px;
-  right: 15%;
+  left: calc(100vw - 35%);
 }
 .bright {
-  right: -110px;
+  left: calc(100vw - 110px);
   top: -70px;
 }
 .bbottom {
   bottom: -110px;
-  right: 10%;
+  left: calc(100vw - 40%);
 }
 .q-page {
   overflow-x: hidden;
   overflow-y: hidden;
+}
+.title {
+  font-family: 'Passion One', cursive;
+  font-weight: bold;
+  font-size: 50pt;
+  color: $primary;
+  z-index: 1;
+}
+.title-container {
+  top: 45vh;
+  left: 71vw;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+}
+.subtitle {
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  font-size: 15pt;
+  align-self: center;
+  color: $secondary;
 }
 </style>
