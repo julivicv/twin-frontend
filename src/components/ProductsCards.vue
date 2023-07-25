c31d797e7ff12dfac047340e3592167f4710396b
julivicv <julivicv@gmail.com>
2023-07-25 05:18:19 -0300

    A

---
+++ src/components/ProductsCards.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tw-grid tw-grid-flow-column tw-grid-cols-5 tw-row-auto tw-gap-4 tw-m-auto tw-">
+    <q-card v-for="p in   productsData  " :key="p.id" class="product-card tw-rounded-lg" rounded>
+      <q-img class="tw-w-[250px] tw-h-[250px] tw-rounded-lg" src="" />
+
+      <q-card-section class="q-py-none tw-py-[7px]">
+        <div class="tw-w-0 tw-h-0">
+          <q-btn round icon="shopping_cart" class="tw-translate-y-[-47px] tw-translate-x-[175px]" color="primary" />
+        </div>
+        <div class="row no-wrap items-center">
+          <div class="col tw-text-[1.05rem] tw-font-bold tw-break-words tw-line-clamp-2">
+            {{ p.name }}
+          </div>
+        </div>
+        <!-- <q-rating v-model="stars" :max="5" size="15px" /> -->
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">{{ p.price }}</div>
+        <div :v-if=p.isFreeShipping class="text-caption text-green">Frete Grátis <q-icon name="local_shipping" />
+        </div>
+
+
+      </q-card-section>
+
+      <q-separator />
+    </q-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface ProductData {
+  id: number,
+  name: string,
+  price: string,
+  isFreeShipping: boolean
+}
+
+export default defineComponent({
+  name: 'ProductsCards',
+  props: { productsData: { type: Array<ProductData> } },
+  setup() {
+    return {
+
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.product-card {
+  width: 250px;
+  background-color: $dark;
+  color: white;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,23 +1,88 @@
 <template>
+  <HeaderTwin />
   <div>
     <div class="q-pa-md row items-start q-gutter-md">
-      <ProductCard />
+      <ProductsCards :productsData="productsData" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ProductCard from '../components/ProductCard.vue';
+import ProductsCards from '../components/ProductsCards.vue';
+import HeaderTwin from 'src/components/HeaderTwin.vue';
 
 export default defineComponent({
   name: 'HomePage',
   components: {
-    ProductCard,
+    ProductsCards,
+    HeaderTwin
   },
   setup() {
     return {
-      ProductCard,
+      ProductsCards,
+      productsData: [
+        {
+          id: 1,
+          name: 'Produto 1',
+          price: 'R$ 120,00',
+          isFreeShipping: true
+        },
+        {
+          id: 2,
+          name: 'Produto 2',
+          price: 'R$ 3550,00',
+          isFreeShipping: false
+        },
+        {
+          id: 3,
+          name: 'Produto 3',
+          price: 'R$ 745,00',
+          isFreeShipping: true
+        },
+        {
+          id: 4,
+          name: 'Produto 4',
+          price: 'R$ 12,00',
+          isFreeShipping: false
+        },
+        {
+          id: 5,
+          name: 'Produto 5',
+          price: 'R$ 23,00',
+          isFreeShipping: true
+        },
+        {
+          id: 6,
+          name: 'Produto 1',
+          price: 'R$ 120,00',
+          isFreeShipping: true
+        },
+        {
+          id: 7,
+          name: 'Produto 2',
+          price: 'R$ 3550,00',
+          isFreeShipping: false
+        },
+        {
+          id: 8,
+          name: 'Produto 3',
+          price: 'R$ 745,00',
+          isFreeShipping: true
+        },
+        {
+          id: 9,
+          name: 'Produto 4',
+          price: 'R$ 12,00',
+          isFreeShipping: true
+        },
+        {
+          id: 0,
+          name: 'Produto 5',
+          price: 'R$ 23,00',
+          isFreeShipping: true
+        },
+      ]
     };
   },
 });
