15816e8249cec52be561c17fab8d38869ba82276
Eduardo Bigolin <eduardobigolin1@hotmail.com>
2023-08-08 20:50:14 -0300

    >:)

--- quasar.config.js
+++ quasar.config.js
@@ -63,7 +63,7 @@ module.exports = configure(function (/* ctx */) {
       // publicPath: '/',
       // analyze: true,
       env: {
-        API: 'https://twin-api.onrender.com',
+        API: 'https://twin-api.onrender.com/api',
       },
       // rawDefine: {}
       // ignorePublicFolder: true,
